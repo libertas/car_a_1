ee85613d59fae299c0c19015aa7ba15cb7afc5ec
libertas <horizonvei@gmail.com>
2016-04-28 20:28:10 +0800

    add image_bfs

--- keil/image.c
+++ keil/image.c
@@ -1,5 +1,10 @@
 #include "image.h"
 
+int image_bfs(bfs_t b, uint16_t height, uint16_t width)
+{
+	return 0;
+}
+
 int image_filter(uint8_t **img, uint16_t height, uint16_t width)
 {
 	
--- keil/image.h
+++ keil/image.h
@@ -1,3 +1,24 @@
 #include <stdint.h>
 
+struct bfs_struct {
+	uint8_t **img;
+	
+	/*
+		0 for white
+		1 for gray
+		2 for black
+	*/
+	uint8_t color;
+	
+	/*
+		-1 for INF
+		-2 for NIL
+	*/
+	int32_t d;
+	int32_t pi;
+};
+
+typedef struct bfs_struct bfs_t;
+
+int image_bfs(bfs_t b, uint16_t height, uint16_t width);
 int image_filter(uint8_t **img, uint16_t height, uint16_t width);
