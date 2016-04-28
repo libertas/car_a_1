44f615d6de083260c851e7a7d6a8d6f3f81c2a79
libertas <horizonvei@gmail.com>
2016-04-28 20:37:03 +0800

    update

--- keil/image.c
+++ keil/image.c
@@ -12,6 +12,15 @@ int image_bfs(bfs_t **b, uint8_t **img, uint16_t height, uint16_t width)
 int image_filter(uint8_t **img, uint16_t height, uint16_t width)
 {
 	bfs_t **b = malloc(height * width);
+	
+	for(uint16_t i = 0; i < height; i++) {
+		for(uint16_t j = 0; j < width; j++) {
+			b[i][j].color = 0;
+			b[i][j].d = -1;
+			b[i][j].pi = -2;
+		}
+	}
+	
 	image_bfs(b, img, height, width);
 	
 	return 0;
