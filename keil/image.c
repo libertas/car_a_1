0887196024de02298e4f12b4e6ef79ce3f5fb497
Libertas <horizonvei@gmail.com>
2016-04-28 22:04:49 +0800

    complete bfs algorithm,not tested

--- keil/image.c
+++ keil/image.c
@@ -1,27 +1,78 @@
-#include <stdlib.h>
-
+#include "camera.h"
 #include "image.h"
 #include "utils.h"
 
+uint16_t bfs_queue_buf[IMAGE_DEFAULT_HEIGHT * IMAGE_DEFAULT_WIDTH];
+bfs_t bfs_buf[IMAGE_DEFAULT_HEIGHT * IMAGE_DEFAULT_WIDTH];
 
-int image_bfs(bfs_t **b, uint8_t **img, uint16_t height, uint16_t width)
+int image_bfs_step(uint16_t u, uint16_t v, bfs_t **b)
 {
+	uint16_t i, j;
+	i = v >> 8;
+	j = v & 0xff;
+	if(b[i][j].color == IMAGE_WHITE) {
+		b[i][j].color = IMAGE_GRAY;
+		b[i][j].d = b[u >> 8][u & 0xff].d + 1;
+		b[i][j].pi = u;
+	}
+	
+	return 0;
+}
+
+int image_bfs(uint8_t **img, uint16_t height, uint16_t width)
+{
+	queue q;
+	uint16_t i, j;
+	uint16_t s, u;
+	
+	for(i = 0; i < height; i++) {
+		for(j = 0; j < width; j++) {
+			init_queue(&q, bfs_queue_buf, IMAGE_DEFAULT_HEIGHT * IMAGE_DEFAULT_WIDTH);
+			
+			bfs_t **b = (bfs_t**)bfs_buf;
+
+			for(uint16_t iii = 0; iii < height; iii++) {
+				for(uint16_t jjj = 0; jjj < width; jjj++) {
+					b[iii][jjj].color = IMAGE_WHITE;
+					b[iii][jjj].d = IMAGE_INF;
+					b[iii][jjj].pi = IMAGE_NIL;
+				}
+			}
+			
+			b[i][j].color = IMAGE_GRAY;
+			b[i][j].d = 0;
+			b[i][j].pi = IMAGE_NIL;
+			
+			s = 0;
+			s |= (i << 8) | j;
+			in_queue(&q, s);
+			
+			while(0 != q.count) {
+				out_queue(&q, &u);
+				if((u >> 8) + 1 < IMAGE_DEFAULT_HEIGHT - 1) {
+					image_bfs_step(u, u + 0x0100, b);
+				}
+				if((u >> 8) - 1 > 0) {
+					image_bfs_step(u, u - 0x0100, b);
+				}
+				if((u & 0xff) + 1 < IMAGE_DEFAULT_WIDTH - 1) {
+					image_bfs_step(u, u + 0x0001, b);
+				}
+				if((u & 0xff + 1) > 0) {
+					image_bfs_step(u, u - 0x0001, b);
+				}
+			}
+		}
+	}
+	
+	
+	
 	return 0;
 }
 
 int image_filter(uint8_t **img, uint16_t height, uint16_t width)
 {
-	bfs_t **b = malloc(height * width);
-	
-	for(uint16_t i = 0; i < height; i++) {
-		for(uint16_t j = 0; j < width; j++) {
-			b[i][j].color = 0;
-			b[i][j].d = -1;
-			b[i][j].pi = -2;
-		}
-	}
-	
-	image_bfs(b, img, height, width);
-	
+	image_bfs(img, height, width);
+
 	return 0;
 }
--- keil/image.h
+++ keil/image.h
@@ -1,5 +1,11 @@
 #include <stdint.h>
 
+#define IMAGE_WHITE (0)
+#define IMAGE_GRAY (1)
+#define IMAGE_BLACK (2)
+#define IMAGE_INF (-1)
+#define IMAGE_NIL (-2)
+
 struct bfs_struct {
 	/*
 		0 for white
@@ -18,5 +24,5 @@ struct bfs_struct {
 
 typedef struct bfs_struct bfs_t;
 
-int image_bfs(bfs_t **b, uint8_t **img, uint16_t height, uint16_t width);
+int image_bfs(uint8_t **img, uint16_t height, uint16_t width);
 int image_filter(uint8_t **img, uint16_t height, uint16_t width);
