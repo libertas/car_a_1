4ce215d07366cd398cf1c7edf725f85c3b5538fd
yiran_fancier <ip28@qq.com>
2016-05-11 22:49:03 +0800

    增加了除干扰的算法

--- my_file/main.c
+++ my_file/main.c
@@ -17,7 +17,7 @@ float g_pre_centroid_x[10] = {0};  //保存十个周期的质心
 
 int main(){
     //数据定义区
-    int i,j,scanline,timeout,erro_n;
+    int i,j,scanline,timeout,erro_n,k;
     int sensor_color_status;
     int hummock_cnt = 0;    //山岗计数
     int sensor_color_antishake = 0;  //颜色传感器防抖
@@ -34,7 +34,9 @@ int main(){
     float tim_ms;
     u16 lcd_id;
     char str_temp[100];
-    image *ov2640_image;
+    image *ov7725_image;
+    u8 up_flag,white_flag,black_flag;
+    u32 up_point,down_point;  //上升沿点，下降沿点
 	u8 **imgd;
     param_struct *param;
     GPIO_TypeDef *gpio_g;
@@ -70,7 +72,7 @@ int main(){
 	
 
     //配置摄像头
-    erro_n = camera_init(&ov2640_image);   //初始化摄像头
+    erro_n = camera_init(&ov7725_image);   //初始化摄像头
     if(erro_n < 0){
         sprintf(str_temp,"camera init error:%d\n",erro_n);
         lcd_show_string(30,100,200,100,16,str_temp);
@@ -131,37 +133,90 @@ int main(){
 /***********************以上是数山岗的部分代码************************/	
 		
 /**********************下面对二值图进行渲染，而且算白线的质心******************/
-		imgd = (u8**)ov2640_image->array;
-		// image_filter(imgd, IMAGE_DEFAULT_HEIGHT, IMAGE_DEFAULT_WIDTH);
-		
-        sum_x = 0;
-        sum_y = 0;
+		//imgd = (u8**)ov2640_image->array;
+		//// image_filter(imgd, IMAGE_DEFAULT_HEIGHT, IMAGE_DEFAULT_WIDTH);
+		//
+        //sum_x = 0;
+        //sum_y = 0;
+        //sum_area = 0;
+        //for(i = 0;i < ov2640_image->height;i++){
+        //scanline = i*(ov2640_image->width);
+        //lcd_set_cursor(0,320 - i);
+        //lcd_ram_write_prepare();
+        //    for(j = 0;j < ov2640_image->width;j++){
+        //        if(((((u8*)imgd)[(scanline + j)/8] << ((scanline + j)%8)) & 0x80) == 0x80){
+        //            lcd_ram_write(0x0);  //渲染二值化图像
+        //        }else{ 
+        //            lcd_ram_write(0xffff);   //渲染二值化图像
+        //            //计算质心
+        //            sum_x = sum_x + j;
+        //            sum_y = sum_y + i;
+        //            sum_area++;
+        //            //计算方差
+        //        }
+        //    }
+        //}
+        ////计算质心
+        //if(sum_area != 0){
+        //    centroid_x = ((float)sum_x)/(float)sum_area;
+        //    centroid_y = ((float)sum_y)/(float)sum_area;
+        //    //计算全局方差
+        //}else{
+        //    centroid_x = 0;
+        //    centroid_y = 0;
+        //}
+
         sum_area = 0;
-        for(i = 0;i < ov2640_image->height;i++){
-        scanline = i*(ov2640_image->width);
-        lcd_set_cursor(0,320 - i);
-        lcd_ram_write_prepare();
-            for(j = 0;j < ov2640_image->width;j++){
-                if(((((u8*)imgd)[(scanline + j)/8] << ((scanline + j)%8)) & 0x80) == 0x80){
+        sum_x = 0;
+        for(i = 0;i < ov7725_image->height;i++){
+            scanline = i*(ov7725_image->width);
+            up_flag = 0;
+            white_flag = 0;
+            black_flag = 0;
+            lcd_set_cursor(ov7725_image->width,320 - i);
+            lcd_ram_write_prepare();
+            for(j = 0;j < ov7725_image->width;j++){
+                if((((ov7725_image->array)[(scanline + j)/8] << ((scanline + j)%8)) & 0x80) == 0x80){ //黑色的
+                    black_flag = 1;
+                    if(white_flag == 1){   //判断为下降升沿
+                        down_point = j;   //记录下降沿点
+                        if(up_flag == 1){   //如果下降沿之前，有上升沿，则两个沿之间可能是白线
+                            if((down_point - up_point > WHITE_LINE_WIDTH_MIN) && (down_point - up_point < WHITE_LINE_WIDTH_MAX)){
+                                //此刻确定是白线
+                                lcd_set_cursor(ov7725_image->width - up_point,320 - i);
+                                lcd_ram_write_prepare();
+                                for(k = up_point;k < down_point;k++){
+                                    sum_area++;
+                                    lcd_ram_write(0xffff);   //渲染二值化图像
+                                    sum_x += k;
+                                }
+                                lcd_set_cursor(ov7725_image->width - j,320 - i);
+                                lcd_ram_write_prepare();
+                            }else{
+                                lcd_ram_write(0x0);  //渲染二值化图像
+                            }
+                        }else{
+                            lcd_ram_write(0x0);  //渲染二值化图像
+                        }
+                    }else{
+                        lcd_ram_write(0x0);  //渲染二值化图像
+                    }
+                    white_flag = 0;
+                }else{ //白色的
+                    white_flag = 1;
+                    if(black_flag == 1){   //判断为上升沿
+                        up_flag = 1;
+                        up_point = j;  //记录上升沿点
+                    }
+                    black_flag = 0;
                     lcd_ram_write(0x0);  //渲染二值化图像
-                }else{ 
-                    lcd_ram_write(0xffff);   //渲染二值化图像
-                    //计算质心
-                    sum_x = sum_x + j;
-                    sum_y = sum_y + i;
-                    sum_area++;
-                    //计算方差
                 }
             }
         }
-        //计算质心
-        if(sum_area != 0){
-            centroid_x = ((float)sum_x)/(float)sum_area;
-            centroid_y = ((float)sum_y)/(float)sum_area;
-            //计算全局方差
-        }else{
+        if(sum_area == 0){
             centroid_x = 0;
-            centroid_y = 0;
+        }else{
+            centroid_x = sum_x/sum_area;
         }
 
         /**********************下面给大车发送白线的质心****************/
--- my_file/main.h
+++ my_file/main.h
@@ -5,5 +5,10 @@
 #define SENSOR_LEFT_LIGHT GPIO_Pin_7
 #define SENSOR_RIGHT_LIGHT GPIO_Pin_3
 #define SENSOR_COLOR GPIO_Pin_1
+#define WHITE_LINE_WIDTH 15
+#define WHITE_LINE_WIDTH_MAX 20   //最大线宽
+#define WHITE_LINE_WIDTH_MIN 5    //最小线宽
+#define STEP_LENGTH 4
+#define WHITE_AREA_SIZE_MIN 200
 
 extern float g_pre_centroid_x[10];
